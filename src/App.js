07abe66e6136675b386b2a2c288c17c600a3f251
cm399 <ideagood64@gmail.com>
2022-03-22 01:30:50 +0500

    Connected api for fetching data

--- src/App.js
+++ src/App.js
@@ -1,68 +1,97 @@
-import React, {useState} from 'react';
-
+import React, {useEffect, useMemo, useState} from 'react';
 import './App.scss';
 import Logo from './images/logo.svg';
 import Tick from './images/tick.png';
+import axios from "axios";
 
 //Bootstrap for responsiveness
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(props) {
 
-    const [steps] = useState([
-        {no: 1, title: 'Order', description: 'Place your Shift 4 order.'},
-        {no: 1, title: 'Underwriting', description: 'Your order is being written.'},
-        {no: 2, title: 'Order Administrator', description: 'Your order is being administrated.'},
-        {no: 3, title: 'Order Services', description: 'You order is under services.'},
-        {no: 4, title: 'Deployment', description: 'Your are project is being deployed.'}
-    ]);
+    const [steps, setSteps] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
 
-    const [currentStep] = useState(3);
+    //get id of the order and pass it to axios to get data
+    const orderId = useMemo(() => '1702e75d-a621-11ec-aaa9-0e1d8cb44bd9', []);
+
+    useEffect(() => {
+        setError('');
+        axios.get(`https://wf-dev.shift4.com/Primary/PostDataToFlow/TaskSystem/OrderTrackerAPI?Status=${orderId}`)
+            .then(res => {
+                if(res.data){
+                    setSteps(res.data);
+                }else {
+                    setError('Data not found!');
+                }
+                setLoading(false);
+            })
+            .catch(err => {
+                setError(err.message);
+                setLoading(false);
+            });
+    }, [orderId]);
 
     return (
         <div className='content'>
             <nav className="navbar navbar-expand-lg navbar-light px-3">
-                <a className="navbar-brand" href="#">
-                    <img style={{width: '144px'}} src={Logo} alt="logo" className='img-fluid' />
+                <a className="navbar-brand" href="/">
+                    <img style={{width: '144px'}} src={Logo} alt="logo" className='img-fluid'/>
                 </a>
             </nav>
-            <div className="content ms-5 mt-5">
+            <div className="content mx-5 mt-5">
+                {loading ? <p>Loading...</p> :
+                    error !== '' ? <p>{error}</p> :
+                        <div className="card col-xl-4 col-lg-6 col-md-12">
+                            <div className='card-header p-4'>
+                                <h2 className='card-title m-0 fw-bold'>Track Order</h2>
+                                <p className='m-0'>Here is what to expect before delivery.</p>
+                                <br />
+                                <p className='mb-0'> <span className='fw-bold me-1'>Order Id</span> {orderId} </p>
+                            </div>
+                            <div className='card-body'>
+                                <div className='steps'>
+                                    <div className='line'/>
 
-                <div className="card col-4">
-                    <div className='card-header d-flex p-4 justify-content-between align-items-center'>
-                        <div>
-                            <h2 className='card-title m-0 fw-bold'>Track Order</h2>
-                            <p className='m-0'>Here is what to expect before delivery.</p>
-                        </div>
-                        <p>Order id # 454674</p>
-                    </div>
-                    <div className='card-body'>
-                        <div className='steps'>
-                            <div className='line' />
-
-                            {steps.map(step => {
-                                return (
-                                    <div key={step.no} className={`step d-flex`}>
-
-                                        {step.no < currentStep ? (
-                                            <div className='circle circle-filled'>
-                                                <div className='icon'>
-                                                    <img src={Tick} alt='T' />
+                                    {steps.map(step => {
+                                        return (
+                                            <div key={step.Index} className='step d-flex'>
+                                                {step.StartTime ? (
+                                                    <div className='circle circle-filled'>
+                                                        <div className='icon'>
+                                                            <img src={Tick} alt='T' />
+                                                        </div>
+                                                    </div>
+                                                ) : (
+                                                    <div className='circle' />
+                                                )}
+                                                <div className='step-text'>
+                                                    <h4 className='m-0'>{step.StageName}</h4>
+                                                    {step.StartTime ?
+                                                        <p>
+                                                            <span className='fw-bold me-1'>Start Time:</span>
+                                                            {new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(new Date(step.StartTime))}
+                                                        </p>
+                                                        : null}
+                                                    {step.CompleteTime ?
+                                                        <p>
+                                                            <span className='fw-bold me-1'>Complete Time:</span>
+                                                            {new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(new Date(step.CompleteTime))}
+                                                        </p>
+                                                        : null}
+                                                    {step.PercentComplete ?
+                                                        <p>{step.PercentComplete}% complete</p>
+                                                        : null}
                                                 </div>
                                             </div>
-                                        ): (
-                                            <div className='circle' />
-                                        )}
-                                        <div className='step-text'>
-                                            <h4 className='m-0'>{step.title}</h4>
-                                            <p>{step.description}</p>
-                                        </div>
-                                    </div>
-                                )
-                            })}
+                                        )
+                                    })}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                }
+                <button className='btn theme-btn mt-4'>Back to Home</button>
             </div>
         </div>
     )
